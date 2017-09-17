7b78282dab78ec7ecb7f7ed964ba13a2daf06b27
Samy VACHERESSE <samy@samyv.com>
2017-09-17 17:46:39 +0200

    add stages resources

---
+++ app/controllers/stages_controller.rb
@@ -0,0 +1,42 @@
+class StagesController < ApplicationController
+  before_action :set_stage, only: [:show, :update, :destroy]
+
+  def index
+    @stages = Stage.all
+  end
+
+  def show
+  end
+
+  def create
+    @stage = Stage.new(stage_params)
+
+    if @stage.save
+      render :show, status: :created, location: @stage
+    else
+      render json: @stage.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @stage.update(stage_params)
+      render :show, status: :ok, location: @stage
+    else
+      render json: @stage.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @stage.destroy
+    head :no_content
+  end
+
+  private
+  def set_stage
+    @stage = Stage.find(params[:id])
+  end
+
+  def stage_params
+    params.fetch(:stage, {})
+  end
+end
---
+++ app/views/stages/_stage.json.jbuilder
@@ -0,0 +1,6 @@
+json.extract! stage,
+  :id,
+  :name,
+  :created_at,
+  :updated_at
+json.url stage_url(stage, format: :json)
