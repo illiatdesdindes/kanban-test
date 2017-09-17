9f347cef9defe52da09018f3946025449085bd0a
Samy VACHERESSE <samy@samyv.com>
2017-09-17 23:40:43 +0200

    seed better data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Stage.create!(name: "À rencontrer")
-Stage.create!(name: "Entretient")
+stages = []
+stages << Stage.create!(name: "À rencontrer")
+stages << Stage.create!(name: "Entretient")
+stages << Stage.create!(name: "Drop")
+stages << Stage.create!(name: "Acceptés")
 
-Applicant.create!(
-  stage: Stage.first,
-  name: "Steve Jobs",
-  job: "Producteur de pommes",
-  avatar: "https://randomuser.me/api/portraits/men/34.jpg"
-)
+
+10.times do 
+  Applicant.create!(
+    stage: stages.sample,
+    name: Faker::Name.name,
+    job: Faker::Job.title,
+    avatar: "https://randomuser.me/api/portraits/men/#{Random.rand(90)}.jpg"
+  )
+end
