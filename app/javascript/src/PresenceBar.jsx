7ed722ce4150744d7862cd6ea9fbf521fbe6e2ce
Samy VACHERESSE <samy@samyv.com>
2017-09-20 03:28:46 +0200

    add presence icons

--- app/javascript/src/PresenceBar.jsx
+++ app/javascript/src/PresenceBar.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { inject, observer } from "mobx-react";
 
+@observer
+class PresenceIcon extends React.Component {
+  render() {
+    const online = this.props.online;
+    let css = online
+      ? "presencebar__icon--online"
+      : "presencebar__icon--offline";
+    css += " presencebar__icon";
+
+    return <span className={css} />;
+  }
+}
+
 @inject("store")
 @observer
 class PresenceBar extends React.Component {
@@ -11,7 +24,10 @@ class PresenceBar extends React.Component {
       <div className="presencebar__component">
         <ul>
           {users.map(user => (
-            <li className="presencebar__user">{user.name}</li>
+            <li key={user.id} className="presencebar__user">
+              <PresenceIcon online={user.online} />
+              {user.name}
+            </li>
           ))}
         </ul>
       </div>
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170919233125) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "avatar"
+    t.boolean "online", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
